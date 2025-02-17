daee12166e9afd6b8831fca52c1ed2c3dd83137e
amyrbakhsh <amyr.bakhsh@gmail.com>
2025-02-17 20:18:20 +0300

    create a new book

--- controllers/books.js
+++ controllers/books.js
@@ -3,14 +3,15 @@ const verifyToken = require("../middleware/verify-token.js");
 const Book = require("../models/Book.js");
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try{
-        req.body.author = req.user._id
-        const hoot = await Book.create(req.body)
-        hoot._doc.author = req.user
-        res.status(201).json(book)
+router.post("/", verifyToken, async (req, res) => {
+    try {
+      req.body.owner = req.user._id;
+      const book = await Book.create(req.body);
+      book._doc.owner = req.user;
+      res.status(201).json(book);
     } catch (err) {
-        res.status(500).json({err: err.message})
+      res.status(500).json({ err: err.message });
     }
-})
+  }
+);
 module.exports = router;
